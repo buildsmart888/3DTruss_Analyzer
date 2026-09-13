a81ad1010d0853227c92e4215492aebf2f1296d2
Phornjed Ph. <buildsmart888@gmail.com>
2026-09-13 19:33:44 +0700

    fix: launch unified engineering workspace

--- 3DTruss_Analyzer_Refactored/src/UI/WinForms/Program.cs
+++ 3DTruss_Analyzer_Refactored/src/UI/WinForms/Program.cs
@@ -2,7 +2,6 @@ namespace TrussAnalyzer.UI.WinForms;
 
 using System;
 using System.Windows.Forms;
-using TrussAnalyzer.UI.AppShell;
 
 /// <summary>
 /// Main entry point for the 3D Truss Analyzer application.
@@ -12,13 +11,6 @@ internal static class Program
     [STAThread]
     private static void Main(string[] args)
     {
-        if (args.Contains("--wpf-shell", StringComparer.OrdinalIgnoreCase))
-        {
-            var application = new System.Windows.Application();
-            application.Run(new GOStructAnalysisShellWindow());
-            return;
-        }
-
         Application.EnableVisualStyles();
         Application.SetCompatibleTextRenderingDefault(true);
         Application.SetHighDpiMode(HighDpiMode.SystemAware);
